c6f0f7513d2587494b5fdbb113e16ae6e7fc6302
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-29 02:58:47 +0500

    Update and rename Script.py.txt to Script.py

---
+++ Script.py
@@ -0,0 +1,24 @@
+# Example, do not modify!
+print(5 / 8)
+
+# Print the sum of 7 and 10
+print(10+7)
+
+# Division
+print(5 / 8)
+
+#Additon
+print(7 + 10)
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100*1.1**7)
