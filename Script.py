a73dd09dc4b1b5ddb38f9f904d5f33b7cfdb33a8
Rizwan Haidar <47399482+rizwanhaidar@users.noreply.github.com>
2020-01-30 01:01:34 +0500

    Update Script.py

--- Script.py
+++ Script.py
@@ -22,3 +22,33 @@ print(4 ** 2)
 
 # How much is your $100 worth after 7 years?
 print(100*1.1**7)
+
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of growth_multiplier and savings to year1
+year1 = savings*growth_multiplier 
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+
+pi_float = float(pi_string)
